d32616a4c45ada6a3c03e668cced0e4204f47125
Vitor-Novaes <vitornovaes27@gmail.com>
2018-01-08 11:39:27 -0300

    second

--- js/application.js
+++ js/application.js
@@ -1,31 +1,59 @@
 alert("Olá Mundo");
 
-var paciente = document.querySelector("#pac-1");
-console.log(paciente.textContent);
+var paciente = document.querySelectorAll("#paciente");
+console.log(paciente);
 
-var peso = paciente.querySelector(".info-peso");
+for(i = 0; i < paciente.length; i++){
+	
+	var peso = paciente[i].querySelector(".info-peso");
+	var altura = paciente[i].querySelector(".info-altura");
+	var imc = paciente[i].querySelector(".info-imc");
+	var status = paciente[i].querySelector(".info-status");
 
-var altura = paciente.querySelector(".info-altura");
+	var pesoValido = true;
+	var alturaValida = true;
 
-var imc = paciente.querySelector(".info-imc");
+	if(peso.textContent <= 1 || peso.textContent >= 500){
+		peso.textContent = "Peso inválido";
+		pesoValido = false;
+		paciente.classList.add("paciente-erro");
+	}
 
-var pesoValido = true;
-var alturaValida = true;
+	if(altura.textContent <= 0.10 || altura.textContent >= 3.00){
+		altura.textContent = "Altura inválida";
+		alturaValida = false;
+		paciente.classList.add("paciente-erro");
+	} 
 
-if(peso.textContent <= 1 || peso.textContent >= 500){
-	peso.textContent = "Peso inválido";
-	pesoValido = false;
+	if(alturaValida && pesoValido){
+		imc.textContent = (peso.textContent / (altura.textContent * altura.textContent));
+		// if(imc.textContent < 18.55){
+		// 	status.textContent = "Abaixo do peso";
+		// }else{
+		// 	if(imc.textContent >= 18.55 || imc.textContent <= 24.99){
+		// 		status.textContent = "Peso normal";
+		// 	}else{
+		// 		if(imc.textContent >= 25 || imc.textContent <= 29.99){
+		// 			status.textContent = "Acima do peso";
+		// 		}else{
+		// 			if(imc.textContent >= 30 || imc.textContent <= 34.99){
+		// 				status.textContent = "Obesidade I";
+		// 			}else{
+		// 				if(imc.textContent >= 35 || imc.textContent <= 39.99){
+		// 					status.textContent = "Obesidade II";
+		// 				}else{
+		// 					if(imc.textContent >= 40){
+		// 						status.textContent = "Obesidade Mórbida";
+		// 					}
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// }
+
+	}else{
+		imc.textContent = "Impossível Calcular";
+	}
 }
 
-if(altura.textContent <= 0.10 || altura.textContent >= 3.00){
-	altura.textContent = "Altura inválida";
-	alturaValida = false;
-} 
-
-if(alturaValida && pesoValido){
-	var imcC = (peso.textContent / (altura.textContent * altura.textContent));	
-	imc.textContent = imcC; 
-}else{
-	imc.textContent = "Impossível Calcular";
-}
 
