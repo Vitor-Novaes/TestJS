a93a364e869bf1355c06ce2278c54acce5262677
Vitor-Novaes <vitornovaes27@gmail.com>
2018-01-08 23:53:47 -0300

    Alfa 1.0

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 alert("Olá Mundo");
 
-var paciente = document.querySelectorAll("#paciente");
+var paciente = document.querySelectorAll(".paciente");
 console.log(paciente);
 
 for(i = 0; i < paciente.length; i++){
@@ -16,40 +16,42 @@ for(i = 0; i < paciente.length; i++){
 	if(peso.textContent <= 1 || peso.textContent >= 500){
 		peso.textContent = "Peso inválido";
 		pesoValido = false;
-		paciente.classList.add("paciente-erro");
+		// paciente.classList.add("paciente-erro");
 	}
 
-	if(altura.textContent <= 0.10 || altura.textContent >= 3.00){
+	if(altura.textContent <= 0.30 || altura.textContent >= 3){
 		altura.textContent = "Altura inválida";
 		alturaValida = false;
-		paciente.classList.add("paciente-erro");
+		// paciente.classList.add("paciente-erro");
 	} 
 
 	if(alturaValida && pesoValido){
-		imc.textContent = (peso.textContent / (altura.textContent * altura.textContent));
-		// if(imc.textContent < 18.55){
-		// 	status.textContent = "Abaixo do peso";
-		// }else{
-		// 	if(imc.textContent >= 18.55 || imc.textContent <= 24.99){
-		// 		status.textContent = "Peso normal";
-		// 	}else{
-		// 		if(imc.textContent >= 25 || imc.textContent <= 29.99){
-		// 			status.textContent = "Acima do peso";
-		// 		}else{
-		// 			if(imc.textContent >= 30 || imc.textContent <= 34.99){
-		// 				status.textContent = "Obesidade I";
-		// 			}else{
-		// 				if(imc.textContent >= 35 || imc.textContent <= 39.99){
-		// 					status.textContent = "Obesidade II";
-		// 				}else{
-		// 					if(imc.textContent >= 40){
-		// 						status.textContent = "Obesidade Mórbida";
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
+		var imc_calculo = (peso.textContent / (altura.textContent * altura.textContent));
+		imc.textContent = imc_calculo.toFixed(2);
+		
+		if(imc_calculo < 18.55){
+			status.textContent = "Abaixo do peso";
+		}else{  
+			if(imc_calculo >= 18.55 || imc_calculo <= 24.99){
+				status.textContent = "Peso normal";
+			}else{
+				if(imc_calculo >= 25 || imc_calculo <= 29.99){
+					status.textContent = "Acima do peso";
+				}else{
+					if(imc_calculo >= 30 || imc_calculo <= 34.99){
+						status.textContent = "Obesidade I";
+					}else{
+						if(imc_calculo >= 35 || imc_calculo <= 39.99){
+							status.textContent = "Obesidade II";
+						}else{
+							if(imc_calculo >= 40){
+								status.textContent = "Obesidade Mórbida";
+							}
+						}
+					}
+				}
+			}
+		}
 
 	}else{
 		imc.textContent = "Impossível Calcular";
